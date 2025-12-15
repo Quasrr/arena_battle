71eb2d3d224ee7baa86d273f0026ac1b983e19e6
TonyLcfOclock <tony.lecerf@oclock.school>
2025-12-15 11:41:37 +0100

    Ajout du CORS au serveur

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import router from './src/routes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// CORS Policy
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
+
 app.use(router);
 
 app.listen(port, () => {
